863703d070a67e8cbe87c6575ef5b232f9c2fc9d
Cosmoden <dennis.lavrushin@gmail.com>
2022-05-02 15:31:07 +0300

    main functions are now operative

--- poi_search.py
+++ poi_search.py
@@ -17,8 +17,6 @@ def find_subcategories(categories, lat, lon, r):
         "key": key
     }
     response = get(url, params=params).json()
-    with open("response.json", 'w') as jsonfile:
-        json.dump(response, jsonfile)
     if not response["results"]:
         return []
     results = response["results"]
@@ -28,3 +26,24 @@ def find_subcategories(categories, lat, lon, r):
         category_set = poi["categorySet"][0]
         subcategories.add(category_set["id"])
     return list(subcategories)
+
+
+def find_poi(category, lon, lat, r):
+    url = "https://api.tomtom.com/search/2/nearbySearch/.json"
+    with open("tomtom_key.txt", 'r') as f:
+        key = f.read()
+    params = {
+        "lat": lat,
+        "lon": lon,
+        "radius": r * 1000,
+        "view": "Unified",
+        "categorySet": category,
+        "relatedPois": "off",
+        "key": key
+    }
+    response = get(url, params=params).json()
+    results = response["results"]
+    for result in results:
+        with open("res.json", 'w') as jsonfile:
+            json.dump(result["poi"], jsonfile)
+        yield result["poi"]
